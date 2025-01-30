05708bf0bb23a2089959c86a959a93557f26b09c
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2025-01-29 22:37:23 -0500

    Initial commit

---
+++ WaveWrangler/Chat.cs
@@ -0,0 +1,45 @@
+using FFXIVClientStructs.FFXIV.Client.System.String;
+using FFXIVClientStructs.FFXIV.Client.UI;
+using System;
+using System.Text;
+
+namespace WaveWrangler
+{
+    // From: https://git.anna.lgbt/anna/XivCommon/src/branch/main/XivCommon/Functions/Chat.cs
+    // And:  https://github.com/Infiziert90/ChatTwo/blob/main/ChatTwo/GameFunctions/ChatBox.cs
+    public unsafe class Chat
+    {
+        public static void SendMessageUnsafe(byte[] message)
+        {
+            var mes = Utf8String.FromSequence(message);
+            UIModule.Instance()->ProcessChatBoxEntry(mes);
+            mes->Dtor(true);
+        }
+
+        public static void SendMessage(string message)
+        {
+            var bytes = Encoding.UTF8.GetBytes(message);
+            if (bytes.Length == 0)
+                throw new ArgumentException("message is empty", nameof(message));
+
+            if (bytes.Length > 500)
+                throw new ArgumentException("message is longer than 500 bytes", nameof(message));
+
+            if (message.Length != SanitiseText(message).Length)
+                throw new ArgumentException("message contained invalid characters", nameof(message));
+
+            SendMessageUnsafe(bytes);
+        }
+
+        private static string SanitiseText(string text)
+        {
+            var uText = Utf8String.FromString(text);
+
+            uText->SanitizeString(0x27F, (Utf8String*)nint.Zero);
+            var sanitised = uText->ToString();
+            uText->Dtor(true);
+
+            return sanitised;
+        }
+    }
+}
---
+++ WaveWrangler/Plugin.cs
@@ -0,0 +1,35 @@
+using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+
+namespace WaveWrangler;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+
+    private const string CommandName = "/o";
+
+    public Plugin()
+    {
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Outputs \"o/\" in chat"
+        });
+    }
+
+    public void Dispose()
+    {
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        Chat.SendMessage("o/");
+        Log.Info("Wave corrected!");
+    }
+}
